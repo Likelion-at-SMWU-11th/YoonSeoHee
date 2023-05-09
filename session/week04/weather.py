9220ababb7ed5e5bc306d6f8c841b1d7bdd31b44
seohee0925 <appleid21@sookmyung.ac.kr>
2023-05-09 20:59:16 +0900

    도시이름, 날씨까지

---
+++ session/week04/weather.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+city = "Seoul"
+apikey = "d906a2e33f857f40b72a5aae6b91d23e"
+api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apikey}"
+
+result = requests.get(api)
+print(result.text)
+
+data = json.loads(result.text)
+
+# print(type(result.text))
+# print(type(data))
+
+print(data["name"], "의 날씨입니다.")
+print("날씨는", data["weather"][0]["main"], "입니다.")
