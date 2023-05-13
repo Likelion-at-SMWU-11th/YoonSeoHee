eb336018b033f8c70396d98c121b6ce8c71bdf46
seohee0925 <appleid21@sookmyung.ac.kr>
2023-05-13 10:11:09 +0900

    21. 날씨 더 자세하게 출력

--- session/week04/weather.py
+++ session/week04/weather.py
@@ -14,7 +14,7 @@ data = json.loads(result.text)
 # print(type(data))
 
 print(data["name"], "의 날씨입니다.")
-print("날씨는", data["weather"][0]["main"], "입니다.")
+print("날씨는", data["weather"][0]["description"], "입니다.")
 print("현재 온도는", data["main"]["temp"], "입니다.")
 print("하지만 체감 온도는", data["main"]["feels_like"], "입니다.")
 print("최저 기온은", data["main"]["temp_min"], "입니다.")
