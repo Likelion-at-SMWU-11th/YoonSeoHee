8e4adf62bfdf8524dcb07da77cc799fa96987964
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-08 17:00:33 +0900

    Fix: view 구현
    lotto 함수 추가

--- assignment/week08/lottos/views.py
+++ assignment/week08/lottos/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+import random
 
 # Create your views here.
+def lotto(request):
+    lotto_number = list()
+    for _ in range(7):
+        lotto_number.append(random.randint(1, 45))
+        
+    return render(request, 'lotto.html', {'lotto_number' : lotto_number})
