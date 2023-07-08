cbcd0fe7afae3144ab38cfb8cd05eb19b076cad9
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-08 17:08:34 +0900

    Feat: Basic mission 구현

--- assignment/week08/lotto/urls.py
+++ assignment/week08/lotto/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lottos.views import lotto
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lotto/', lotto, name='lotto'),
 ]
