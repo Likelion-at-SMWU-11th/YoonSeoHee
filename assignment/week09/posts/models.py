c4e904ac15d2d0d658978563e0199fcc9f60d812
seohee0925 <appleid21@sookmyung.ac.kr>
2023-07-17 17:03:30 +0900

    [Feat] model 구현

--- assignment/week09/posts/models.py
+++ assignment/week09/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField(verbose_name='이미지')
+    contentn = models.TextField('내용')
+    created_at = models.DateTimeField('작성일', auto_now_add=True)
+    view_count = models.IntegerField('조회수', default=0)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='작성자', null=True, blank=True)
