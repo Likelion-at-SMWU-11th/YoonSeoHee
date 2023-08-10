9239958ca6219ca6e87a80b2d6a55808c37d0444
윤서희 <appleid21@sookmyung.ac.kr>
2023-08-10 19:01:26 +0900

    [Feat] 13주차 과제

--- session/week10/LikeLion11th/week10_session/accounts_token/views.py
+++ session/week10/LikeLion11th/week10_session/accounts_token/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny]) # 토큰없이 누구나 성공
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request,
+                        username = username,
+                        password = password)
+    
+    if user: # 로그인 성공 시 토큰 반환
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response ({'token' : token.key})
+    else: # 로그인 실패시 401 에러 반환
+        return Response(status=401)
