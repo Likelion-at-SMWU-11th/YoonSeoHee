22774225665312b8f4058c45278baaadc6915141
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-14 20:06:05 +0900

    [Feat] 5주차 세션

--- session/week04_2/auth/src/main/java/com/example/auth/config/WebSecurityConfig.java
+++ session/week04_2/auth/src/main/java/com/example/auth/config/WebSecurityConfig.java
@@ -20,12 +20,24 @@ public class WebSecurityConfig {
                                 .requestMatchers("/home/**") // home 페이지에 들어가는 URL에 대한 접근 권한 설정
                                 .anonymous()
                                 .anyRequest()
-                                .authenticated()
+                                .authenticated() // 아래가 없다면 anyRequest()에 걸려서 여기를 거침
+                                )
+                                .formLogin(login -> login
+                                        .loginPage("/user/login")
+                                        .defaultSuccessUrl("/home") // 로그인 성공 시 돌아가는 페이지 (default로 설정)
+                                        .permitAll() // authorize 요청 무시
+                                )
+                                .logout(logout -> logout
+                                        .logoutUrl("/user/logout")
+                                        .logoutSuccessUrl("/home") // 로그아웃 성공시 돌아가는 페이지
+                                        .deleteCookies("JSESSIONID") // 쿠키 삭제
+                                        .invalidateHttpSession(true) // http 세션 객체에 저장해두었던 내용 삭제
+                                        .permitAll()
+                                );
                                 // permitAll: Specify that URLs are allowed by anyone.
                                 // authenticated: Specify that URLs are allowed by any authenticated user.
                                 // anonymous: Specify that URLs are allowed by anonymous users.
-                )
-                .formLogin(withDefaults());
+
 
         return http.build();
     }
